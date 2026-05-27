e5a6b669edaeedf0c49dedb6075c6df8500312a4
Ronald Gosso <ronald.gosso@neptuneprolabs.com>
2026-05-27 13:58:25 +0300

    Add me endpoint to get current user info

--- api/me.php
+++ api/me.php
@@ -1,1 +1,8 @@
 <?php
+require_once __DIR__ . '/../includes/functions.php';
+
+$user = getCurrentUser();
+if (!$user) {
+    jsonResponse(['error' => 'Not authenticated'], 401);
+}
+jsonResponse(['user' => $user]);
